d27e09a132d9d41959865615a6c5daa49df02c31
Anthony <63381943+4nth0nySLT@users.noreply.github.com>
2020-04-12 22:47:54 -0500

    Update test.py

--- test.py
+++ test.py
@@ -18,15 +18,32 @@ from time import sleep
 ##################################
 """
 try:
-    print("Ingresa tu diccionario:")
-    dic=input("")
-    print("Nombre del HandShake:")
-    pcap=input("")
+    print("""
+1) Diccionario
+2) Crunch""")
+    x=input()
+    while True:
+        if x=="1":
+            print("Ingresa tu diccionario")
+            dic=input("")
+            print("Nombre del HandShake:")
+            pcap=input("")
+            break
+        elif x=="2":
+            dic=""
+            print("""Numeros de 10 digitos""")
+            x=input("Prefijo:")
+            print("Nombre del HandShake, debe de terminar en _<MAC>.cap, cada par separado por '-' o ':'")
+            pcap=input("")
+            break
+        else:
+            print("Escoge bien")
+            
     print("Primero un test de velocidad, espera por favor...")
     print(cmd.test())
-    sleep(3)
+
     print("Listo, comencemos")
-    cmd.wpa(dic,pcap)
+    cmd.wpa(dic,pcap,x)
     exit()
 
 except Exception as e:
